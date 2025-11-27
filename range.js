6e4dd47270a8eb548b4a259af542e46599c762fb
Mohamed Hmza <mhmza.swe@gmail.com>
2025-11-26 19:34:44 -0500

    filter range problem

---
+++ range.js
@@ -0,0 +1,12 @@
+function filterRange(arr, a, b) {
+    return arr
+    .filter((num) => num >= a && num <= b)
+}
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (matching values)
+
+alert( arr ); // 5,3,8,1 (not modified)
